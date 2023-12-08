c41c9e31903d2554f617f9fae6c10fe0715eacaf
haris <89023322+Haresz@users.noreply.github.com>
2023-12-08 15:44:30 +0700

    menambahkan  component Headline dan stylenya

---
+++ src/component/Headline.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Headline(props) {
+  return <p className="headline">{props.name}</p>;
+}
+
+export default Headline;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./style/style.scss";
+import Headline from "./component/headline";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <p>"HALO WORLD"</p>
-);
+const root = createRoot(document.getElementById("root"));
+root.render(<Headline name="REGISTER" />);
