00e4f4a0a4bf659df54af99f49e4c19565e30174
haris <89023322+Haresz@users.noreply.github.com>
2023-12-09 12:17:49 +0700

    style LoginPage dan resize

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,10 +8,10 @@ function LoginPage() {
     <div className="container-relog">
       <div className="form">
         <Headline name="LOGIN" />
-        <p>Welcome to capcin always for you</p>
+        <h2>Welcome to capcin always for you</h2>
         <Input placeholder="Enter Username" />
         <Input placeholder="Password" />
-        <div className="">
+        <div className="container__checkbox-password">
           <div className="checkbox">
             <input type="checkbox" name="remembr" id="remember" />
             <label htmlFor="remember">Remember me</label>
@@ -19,14 +19,14 @@ function LoginPage() {
           <p>Forgot password ?</p>
         </div>
         <ButtonFill name="Submit" />
-        <p>Or continou with</p>
+        <p className="continou-with">Or continue with</p>
         <div className="acount">
           <img src="public/GmailGmail.svg" alt="" />
           <img src="public/Microsoft.svg" alt="" />
           <img src="public/Facebook.svg" alt="" />
           <img src="public/Apple.svg" alt="" />
         </div>
-        <p>Don't have a account ? Click here</p>
+        <p className="text-reg">Don't have a account ? Click here</p>
       </div>
       <img src="public/relog.jpeg" alt="" />
     </div>
