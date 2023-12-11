7a6e108e47c5a880af0c4735daa131203f3f62d9
haris <89023322+Haresz@users.noreply.github.com>
2023-12-11 10:36:20 +0700

    menambahkan data dumy

---
+++ src/utils/data.js
@@ -0,0 +1,50 @@
+const dataBook = [
+  {
+    image: "public/book/book-1.png",
+    title: "How to Win Friends & Influence People",
+    author: "Dale Carnegie",
+    isRecommended: true,
+  },
+  {
+    image: "public/book/book-2.png",
+    title: "MBA",
+    author: "Jason Barron",
+    isRecommended: true,
+  },
+  {
+    image: "public/book/book-3.png",
+    title: "The Intelligent Investor",
+    author: "Benjamin Graham",
+    isRecommended: true,
+  },
+  {
+    image: "public/book/book-4.png",
+    title: "Great At Work",
+    author: "Morten T.Hansen",
+    isRecommended: true,
+  },
+  {
+    image: "public/book/book-5.png",
+    title: "You Do You",
+    author: "Fellexandra Ruby",
+    isRecommended: false,
+  },
+  {
+    image: "public/book/book-6.png",
+    title: "Ikigai",
+    author: "Hector Garcia & Frances...",
+    isRecommended: false,
+  },
+  {
+    image: "public/book/book-7.png",
+    title: "How To Be A Stoic",
+    author: "Massimo Pigliucci",
+    isRecommended: false,
+  },
+  {
+    image: "public/book/book-8.png",
+    title: "The Subtle Not Giving A F*ck",
+    author: "Mark Manson",
+    isRecommended: false,
+  },
+];
