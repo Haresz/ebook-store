aa527e78839b37150f2593c81f6f1de69b83e97e
haris <89023322+Haresz@users.noreply.github.com>
2023-12-13 18:48:49 +0700

    mengubah isi index

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./style/style.scss";
-import PopularPage from "./pages/PopularPage";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<PopularPage />);
+root.render(<></>);
