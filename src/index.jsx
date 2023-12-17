132f9e4a4c6dc6071eccd67b5d07e57f56a7b639
haris <89023322+Haresz@users.noreply.github.com>
2023-12-17 08:41:28 +0700

    menambahkan rote path

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./style/style.scss";
+import Dashboard from "./pages/Dashboard";
+import SubMenuPage from "./pages/SubMenuPage";
 import DetailBook from "./pages/DetailBook";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/submenu",
+    element: <SubMenuPage />,
+  },
+  {
+    path: "/detailBook",
+    element: <DetailBook />,
+  },
+]);
+
 const root = createRoot(document.getElementById("root"));
-root.render(<DetailBook />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
